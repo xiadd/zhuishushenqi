659c1dcdb07d3b95cecb733ff37202ada963033e
xiadd <xiadd0102@gmail.com>
2017-04-23 13:46:30 +0800

    build

--- config/config.default.js
+++ config/config.default.js
@@ -1,6 +1,6 @@
 module.exports = {
   DBURL: 'mongodb://localhost:27017/test',
   JWTSecret: 'xiadd',
-  WXSecret: 'a306e94c6159ed9fb508e9028d1b54c1',
-  WXAppId: 'wx23626c30e27959b9'
+  WXSecret: '***',
+  WXAppId: '***'
 }
