2900566e1f193f0d34dcdf45f82f1a3ce1a15df8
xiadd <xiadd0102@gmail.com>
2017-05-31 21:22:06 +0800

    build

--- src/app.js
+++ src/app.js
@@ -2,22 +2,25 @@ import Koa from 'koa'
 import path from 'path'
 import views from 'koa-views'
 import serve from 'koa-static'
-import log4js from 'koa-log4'
+import morgan from 'koa-morgan'
+import fs from 'fs'
 import bodyParser from 'koa-bodyparser'
 import config from '../config/config.default'
 import DB from './model'
 import router from './router'
 import { setCorrectResponse, setCors } from './middleware'
-import './lib/logger'
+
+const accessLogStream = fs.createWriteStream(path.resolve(__dirname, '../logs/access.log'), { flags: 'a' })
 
 const app = new Koa()
+
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.context.config = config
 //中间件
 app.use(serve(path.resolve('./static')))
 
 app.use(bodyParser())
-app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }))
 app.use(setCorrectResponse())
 app.use(setCors())
 app.use(router.routes()).use(router.allowedMethods())
