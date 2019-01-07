1a77d5332ff330f94d69ab8ae10c8b7b3e74c76c
xiadd <xiadd0102@gmail.com>
2019-01-07 14:01:17 +0800

    test: 增加测试

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+const supertest = require('supertest')
+const { app } = require('../src/app')
+const request = supertest(app.callback())
+
+test('服务器启动', async () => {
+  const response = await request.get('/')
+  expect(response.status).toBe(404)
+})
