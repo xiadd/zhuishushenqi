0e4358f2b7cd43b04e5f24daf539f95c37ba9e46
xiadd <xiadd0102@gmail.com>
2019-05-07 13:18:46 +0800

    fix url

--- pages/index/index.jsx
+++ pages/index/index.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Collapse, Icon, Button } from 'antd'
+import Link from 'next/link'
 import { StyledIndex } from './index.styled'
 
 const Panel = Collapse.Panel
 
 class Index extends Component {
 
-  // static async getInitialProps () {
-  //   const systemInfo = await axios.get('/system')
-  //   return systemInfo.data
-  // }
+  static async getInitialProps () {
+    const systemInfo = await axios.get('http://0.0.0.0:8080/system')
+    return systemInfo.data
+  }
 
   render() {
     return <StyledIndex>
@@ -28,7 +29,7 @@ class Index extends Component {
         Made by <a href="https://github.com/xiadd" target="_blank" rel="noopener noreferrer">xiadd</a>
       </p>
 
-      {/* <Collapse
+      <Collapse
         bordered={false}
         expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
       >
@@ -69,7 +70,7 @@ class Index extends Component {
               <span className="debug-value" dangerouslySetInnerHTML={{ __html: this.props.hotRoutesValue }} />
             </div>
         </Panel>
-      </Collapse> */}
+      </Collapse>
     </StyledIndex>
   }
 }
