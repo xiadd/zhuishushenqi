6f023b500adde298dba7f2246a3e9a42b7d45730
xiadd <xiadd0102@gmail.com>
2020-01-15 18:40:22 +0800

    首页fix

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,8 +3,7 @@ const Controller = require('egg').Controller
 class HomeController extends Controller {
   async index() {
     const { ctx } = this
-    const res = await ctx.service.novel.list({})
-    await ctx.render('home.nj', { books: res.books || [] })
+    await ctx.render('home.nj')
   }
 }
 
