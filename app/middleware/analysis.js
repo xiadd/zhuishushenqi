d4a897000d78a2ebdc77a02dc9561e2d7e806026
xiadd <xiadddd@icloud.com>
2020-02-04 11:14:39 +0800

    change ip

--- app/middleware/analysis.js
+++ app/middleware/analysis.js
@@ -1,7 +1,7 @@
 module.exports = (options, app) => {
   return async function analysis (ctx, next) {
     await next()
-    console.log(ctx.ip)
+    console.log(ctx.request.ip)
     const v = await app.redis.get(ctx.url)
     app.redis.set(ctx.url, v ? +v + 1 : 1)
   }
