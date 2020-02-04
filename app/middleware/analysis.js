8821f49c58ba5c419fde4274773e21cb7d8b0703
xiadd <xiadddd@icloud.com>
2020-02-04 10:55:54 +0800

    初步增加统计

--- app/middleware/analysis.js
+++ app/middleware/analysis.js
@@ -2,5 +2,7 @@ module.exports = (options, app) => {
   return async function analysis (ctx, next) {
     await next()
     console.log(ctx.ip)
+    const v = await app.redis.get(ctx.url)
+    app.redis.set(ctx.url, v ? +v + 1 : 1)
   }
 }
