8251e151f0ce236350075c074a090c5f42b1a21f
xiadd <xiadddd@icloud.com>
2020-03-26 17:42:58 +0800

    移除统计

--- config/config.default.js
+++ config/config.default.js
@@ -22,15 +22,6 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
-  config.redis = {
-    client: {
-      port: process.env.REDIS_PORT || 6379,          // Redis port
-      host: process.env.REDIS_HOST || '127.0.0.1',   // Redis host
-      password: '',
-      db: 0,
-    },
-  }
-
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
