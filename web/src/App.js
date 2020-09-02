ed04289c990fa533b602a3072409b4af1fa9a9eb
xiadd <xiadddd@icloud.com>
2020-09-02 21:09:06 +0800

    文档修复

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          后面文档使用react进行开发(USING REACT OR VUE!!!)
-        </a>
-      </header>
+      <h1>zhuishushenqi</h1>
+      <p>修整中</p>
     </div>
   );
 }
